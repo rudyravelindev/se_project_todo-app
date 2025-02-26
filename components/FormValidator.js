a4180af25af1ae715e6d470f4c8a9228c87dadfe
rudyravelindev <rudyravelindev@gmail.com>
2025-02-25 21:51:05 -0800

    Part 7: Form Validator Part 2  and final part done

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -1,12 +1,82 @@
 class FormValidation {
   constructor(settings, formEl) {
     this._inputSelector = settings.inputSelector;
+    this._formSelector = settings.formSelector;
     this._submitButtonSelector = settings.submitButtonSelector;
     this._errorClass = settings.errorClass;
-    this._inputErrorClass = settings.nputErrorClass;
+    this._inputErrorClass = settings.inputErrorClass;
     this._inactiveButtonClass = settings.inactiveButtonClass;
+    this._formEl = formEl;
+
+    this._submitButton = this._formEl.querySelector(this._submitButtonSelector);
+    this._inputList = Array.from(
+      this._formEl.querySelectorAll(this._inputSelector)
+    );
   }
-  enableValidation() {}
+
+  _showInputError(inputElement, errorMessage) {
+    const errorElement = this._formEl.querySelector(
+      `#${inputElement.id}-error`
+    );
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(this._errorClass);
+  }
+
+  _hideInputError(inputElement) {
+    const errorElement = this._formEl.querySelector(
+      `#${inputElement.id}-error`
+    );
+    inputElement.classList.remove(this._inputErrorClass);
+    errorElement.classList.remove(this._errorClass);
+    errorElement.textContent = '';
+  }
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._submitButton.classList.add(this._inactiveButtonClass);
+      this._submitButton.disabled = true;
+    } else {
+      this._submitButton.classList.remove(this._inactiveButtonClass);
+      this._submitButton.disabled = false;
+    }
+  }
+
+  _hasInvalidInput() {
+    return this._inputList.some((inputElement) => !inputElement.validity.valid);
+  }
+
+  _setEventListeners() {
+    this._toggleButtonState();
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
+
+  enableValidation() {
+    this._formEl.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
+  resetValidation() {
+    this._inputList.forEach((inputElement) => {
+      this._hideInputError(inputElement);
+    });
+    this._toggleButtonState();
+  }
 }
 
 export default FormValidation;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
-
 import { initialTodos, validationConfig } from '../utils/constants.js';
 import Todo from '../components/Todo.js';
 import FormValidation from '../components/FormValidator.js';
-// instantiate
+
 const addTodoButton = document.querySelector('.button_action_add');
 const addTodoPopup = document.querySelector('#add-todo-popup');
 const addTodoForm = addTodoPopup.querySelector('.popup__form');
+
 const addTodoCloseBtn = addTodoPopup.querySelector('.popup__close');
-//const todoTemplate = document.querySelector('#todo-template'); TO BE REMOVED
 const todosList = document.querySelector('.todos__list');
 
 const openModal = (modal) => {
@@ -24,34 +23,6 @@ const generateTodo = (data) => {
   const todo = new Todo(data, '#todo-template');
   const todoElement = todo.getView();
   return todoElement;
-  //TO BE REMOVED
-  // const todoElement = todoTemplate.content
-  //   .querySelector('.todo')
-  //   .cloneNode(true);
-  // const todoNameEl = todoElement.querySelector('.todo__name');
-  // const todoCheckboxEl = todoElement.querySelector('.todo__completed');
-  // const todoLabel = todoElement.querySelector('.todo__label');
-  // const todoDate = todoElement.querySelector('.todo__date');
-  // const todoDeleteBtn = todoElement.querySelector('.todo__delete-btn');
-  // todoNameEl.textContent = data.name;
-  // todoCheckboxEl.checked = data.completed;
-  // // Apply id and for attributes.
-  // // The id will initially be undefined for new todos.
-  // todoCheckboxEl.id = `todo-${data.id}`;
-  // todoLabel.setAttribute('for', `todo-${data.id}`);
-  // // If a due date has been set, parsing this it with `new Date` will return a
-  // // number. If so, we display a string version of the due date in the todo.
-  // const dueDate = new Date(data.date);
-  // if (!isNaN(dueDate)) {
-  //   todoDate.textContent = `Due: ${dueDate.toLocaleString('en-US', {
-  //     year: 'numeric',
-  //     month: 'short',
-  //     day: 'numeric',
-  //   })}`;
-  // }
-  // todoDeleteBtn.addEventListener('click', () => {
-  //   todoElement.remove();
-  // });
 };
 
 addTodoButton.addEventListener('click', () => {
@@ -77,12 +48,20 @@ addTodoForm.addEventListener('submit', (evt) => {
   const todo = generateTodo(values);
   todosList.append(todo);
   closeModal(addTodoPopup);
+
+  // Reset form validation after submission
+  newTodoValidator.resetValidation();
+  addTodoForm.reset(); // Clear form fields
 });
 
+// Render initial todos
 initialTodos.forEach((item) => {
   const todo = generateTodo(item);
   todosList.append(todo);
 });
 
+// Declare and initialize newTodoValidator before using it
 const newTodoValidator = new FormValidation(validationConfig, addTodoForm);
+
+// Now you can safely call enableValidation
 newTodoValidator.enableValidation();
